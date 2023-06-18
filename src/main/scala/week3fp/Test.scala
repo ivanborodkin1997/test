b5c14400f0e452d676fb22363f17b1414f1f412d
ivanborodkin1997 <boroda0360@mail.ru>
2023-06-18 19:44:33 +0300

    test

--- src/main/scala/week3fp/Test.scala
+++ src/main/scala/week3fp/Test.scala
@@ -2,14 +2,24 @@ package week3fp
 
 object Test extends App {
 
-  var some = SomeTest(1,"someTest", Some(7))
+  var some = SomeTest(1, "someTest", Some(7))
+  var example = SomeTest(2, "exampleTest", Some(0))
 
-  val result: String = some match {
-    case SomeTest(i,d,k) if k.isEmpty => "неизвестно"
-    case SomeTest(i,d,k) if k.get == 0 => "неизвестно"
-    case SomeTest(i,d,k) if k.get % 2 == 0 => "четное"
-    case SomeTest(i,d,k) if k.get % 2 == 1 => "нечетное"
+  def someResult(example: SomeTest): String = example match {
+    case SomeTest(i, d, k) if k.isEmpty => "неизвестно"
+    case SomeTest(i, d, k) if k.get == 0 => "неизвестно"
+    case SomeTest(i, d, k) if k.get % 2 == 0 => "четное"
+    case SomeTest(i, d, k) if k.get % 2 == 1 => "нечетное"
   }
+
+  //немного другой вариант
+  val result: String = some match {
+    case SomeTest(i, d, k) if k.isEmpty => "неизвестно"
+    case SomeTest(i, d, k) if k.get == 0 => "неизвестно"
+    case SomeTest(i, d, k) if k.get % 2 == 0 => "четное"
+    case SomeTest(i, d, k) if k.get % 2 == 1 => "нечетное"
+  }
+  println(someResult(example))
   print(result)
 }
 
--- src/main/scala/week3fp/Test2.scala
+++ src/main/scala/week3fp/Test2.scala
@@ -2,9 +2,9 @@ package week3fp
 
 object Test2 extends App {
 
-  val seq = Seq(1,2,3,4,5,6,7,8,9,10,11,12,13)
+  val seq = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
   print(seq.filter(_ % 2 == 1).sum)
 
   // таким образом будет еще короче
-  print(Seq(1,2,3,4,5,6,7,8,9,10,11,12,13,14).filter(_ % 2 == 1).sum)
+  print(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14).filter(_ % 2 == 1).sum)
 }
