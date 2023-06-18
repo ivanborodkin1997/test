0f8c5583c13f32258976956983c570fc675638de
ivanborodkin1997 <boroda0360@mail.ru>
2023-06-18 19:54:53 +0300

    test

--- src/main/scala/week3fp/Test.scala
+++ src/main/scala/week3fp/Test.scala
@@ -2,10 +2,11 @@ package week3fp
 
 object Test extends App {
 
-  var some = SomeTest(1, "someTest", Some(7))
-  var example = SomeTest(2, "exampleTest", Some(0))
+  //объекты для проверки
+  var example = SomeTest(1, "exampleTest", Some(0))
+  var anotherExample = SomeTest(2, "someTest", Some(7))
 
-  def someResult(example: SomeTest): String = example match {
+  def result(example: SomeTest): String = example match {
     case SomeTest(i, d, k) if k.isEmpty => "неизвестно"
     case SomeTest(i, d, k) if k.get == 0 => "неизвестно"
     case SomeTest(i, d, k) if k.get % 2 == 0 => "четное"
@@ -13,14 +14,17 @@ object Test extends App {
   }
 
   //немного другой вариант
-  val result: String = some match {
+  val anotherResult: String = anotherExample match {
     case SomeTest(i, d, k) if k.isEmpty => "неизвестно"
     case SomeTest(i, d, k) if k.get == 0 => "неизвестно"
     case SomeTest(i, d, k) if k.get % 2 == 0 => "четное"
     case SomeTest(i, d, k) if k.get % 2 == 1 => "нечетное"
   }
-  println(someResult(example))
-  print(result)
+
+  //вывод результата
+  println(result(example))
+  println(anotherResult)
+
 }
 
 case class SomeTest(id: Int, description: String, kind: Option[Int]) {
