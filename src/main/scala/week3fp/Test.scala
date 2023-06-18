f200ef5148d3e1205ca88fb666d477270ad667d2
ivanborodkin1997 <boroda0360@mail.ru>
2023-06-18 14:32:59 +0300

    test

--- src/main/scala/week3fp/Test.scala
+++ src/main/scala/week3fp/Test.scala
@@ -4,13 +4,13 @@ object Test extends App {
 
   var some = SomeTest(1,"someTest", Some(0))
 
-  val description: String = some match {
+  val result: String = some match {
     case SomeTest(i,d,k) if k.isEmpty => "неизвестно"
     case SomeTest(i,d,k) if k.get == 0 => "неизвестно"
     case SomeTest(i,d,k) if k.get % 2 == 0 => "четное"
     case SomeTest(i,d,k) if k.get % 2 == 1 => "нечетное"
   }
-  print(description)
+  print(result)
 }
 
 case class SomeTest(id: Int, description: String, kind: Option[Int]) {
